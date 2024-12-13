46cbeb90bc3263c1c71f782e231a5c9245e3418e
Mark Scannell <scannell@google.com>
2024-12-13 16:32:37 +0000

    Renamed args to app.

--- simulator/src/main/java/com/google/cloud/App.java
+++ simulator/src/main/java/com/google/cloud/App.java
@@ -78,23 +78,23 @@ public class App {
   public static void main(String argv[]) {
 
     // Initialize the parsing objects
-    App args = new App();
+    App app = new App();
     JCommander parser = JCommander
         .newBuilder()
-        .addObject(args)
+        .addObject(app)
         .build();
 
     try {
 
       // Parse the user-specifiedc arguments
       parser.parse(argv);
-      if (args.help) {
+      if (app.help) {
         parser.usage();
         System.exit(1);
       }
 
       // Run the simulator
-      args.runSimulator();
+      app.runSimulator();
     }
 
     // Failure parsing an argument (show usage)
